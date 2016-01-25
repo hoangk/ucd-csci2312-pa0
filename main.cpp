1087c0e5d7eb2a9559684d97b972a29c5f658a3f
hoangk <Khoa.Hoang@Ucdenver.edu>
2016-01-24 22:50:01 -0700

    initial commit
    I made it

--- main.cpp
+++ main.cpp
@@ -3,12 +3,14 @@
  * Project: ucd-csci2312-pa0
  * Author: Ivo Georgiev
  * Date: 2016-01-18
+ * Edit: 2016-01-24
+ * Khoa Hoang
  */
 #include <iostream>
 
 int main() {
-    // TODO Substitute 11 with your UCD ID number, modulo 13 (myId % 13)
-    std::cout << "Hello, world! My number is " << 11 << '!' << std::endl;
+
+    std::cout << "Hello, world! My number is " << 103307195 % 13 << '!' << std::endl;
 
     return 0;
 }
